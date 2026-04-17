1c63f308b9a0cd485982b4e8df22a7a14f871b5a
RidwanAbdulkadir <olayinkaridwan20@gmail.com>
2026-04-17 21:49:41 +0100

    update

--- example1.py
+++ example1.py
@@ -1,6 +1,7 @@
 '''
 This is an example of a synchronous Python script. 
 '''
+import asyncio
 import time
 
 def fetch_data():
@@ -26,4 +27,29 @@ print("Results:", results)
 t2 = time.time()
 print(f"Script completed in {t2 - t1:.2f} seconds.")
 
+'''
+Example of an asynchronous code in python below
+'''
+
+async def fetch_data_async():
+    print("Fetching data asynchronously...")
+    await asyncio.sleep(2)  # Simulate a delay in fetching data
+    print("Data fetched successfully!")
+    return f"Results of async data fetching at {time.ctime()}"
+
+async def main_async():
+    task1 = fetch_data_async()
+    task2 = fetch_data_async()
+    results = await asyncio.gather(task1, task2)
+    print("Task 1 completed successfully.")
+    print("Task 2 completed successfully.")
+    return results
+
+t1 = time.time()
+
+results_async = asyncio.run(main_async())
+print("Results:", results_async)
+
+t2 = time.time()
+print(f"Async script completed in {t2 - t1:.2f} seconds.")
 
